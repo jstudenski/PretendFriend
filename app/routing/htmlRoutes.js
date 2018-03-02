7f073cf15c9ce1285a25596b850877e26a706de9
jstudenski <jstudenski@gmail.com>
2018-03-01 18:19:18 -0600

    testing bulma template

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,6 +14,9 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/view.html"));
   });
 
+  app.get("/test", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/test.html"));
+  });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
