8acdb45784e1613ed8694f75fe78b00670b49892
jstudenski <jstudenski@gmail.com>
2018-03-03 09:49:44 -0600

    modal

--- app/public/survey.js
+++ app/public/survey.js
@@ -127,11 +127,6 @@ $(".submit").on("click", function(event) {
 
         }
       });
-      // Clear the form when submitting
-      // $("#reserve-name").val("");
-      // $("#reserve-phone").val("");
-      // $("#reserve-email").val("");
-      // $("#reserve-unique-id").val("");
 
     });
 
@@ -157,3 +152,30 @@ function diffCheck(arr1, arr2) {
 }
 
 diffCheck(myArray, computerArray);
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
