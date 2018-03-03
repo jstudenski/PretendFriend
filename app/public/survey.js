bf98f931fd726cc7237445f04f636f7c0d674fac
jstudenski <jstudenski@gmail.com>
2018-03-03 12:48:44 -0600

    real questions added

--- app/public/survey.js
+++ app/public/survey.js
@@ -26,7 +26,18 @@ function randomName() {
 }
 
 // generate questions html
-var questions = ['Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5', 'Question 6', 'Question 7', 'Question 8', 'Question 9', 'Question 10',];
+var questions = [
+'Your mind is always buzzing with unexplored ideas and plans.', 
+'Generally speaking, you rely more on your experience than your imagination.', 
+'You find it easy to stay relaxed and focused even when there is some pressure.', 
+'You rarely do something just out of sheer curiosity.', 
+'People can rarely upset you.', 
+'It is often difficult for you to relate to other people’s feelings.', 
+'In a discussion, truth should be more important than people’s sensitivities.', 
+'You rarely get carried away by fantasies and ideas.', 
+'You think that everyone’s views should be respected regardless of whether they are supported by facts or not.', 
+'You feel more energetic after spending time with a group of people.'
+];
 for (var i = 0; i < questions.length; i++) {
   $('.radio-questions').append(questions[i] + '<br>');
   var name = 'question' + i;
@@ -44,7 +55,7 @@ for (var i = 0; i < questions.length; i++) {
     }
     label.appendTo($('.radio-questions'));
   }
-  $('.radio-questions').append('<br>');
+  $('.radio-questions').append('<br><br>');
 }
 
 function randomNum(min, max) {
