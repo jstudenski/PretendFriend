c5534fdf4fa93e1a632b2dbf4503a60a67afb6e0
jstudenski <jstudenski@gmail.com>
2018-03-02 22:40:13 -0600

    remove unused routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,51 +11,10 @@ module.exports = function(app) {
     res.json(randomNames);
   });
 
-// // Create New Reservation - takes in JSON input
-// app.post("/api/new", function(req, res) {
-
-//   var newReservation = req.body;
-
-//   console.log(newReservation);
-
-//   reservations.push(newReservation);
-
-//   res.json(newReservation);
-// });
-
-
-
   app.post("/api/friends", function(req, res) {
-
-    //var newFriend = req.body;
-    //console.log(newFriend);
-    //friendData.push(newFriend);
-    //res.json(friendData);
     console.log(req.body);
     friendData.push(req.body);
     res.json(friendData);
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-    // if (tableData.length < 5) {
-    //   tableData.push(req.body);
-    //   res.json(true);
-    // }
-    // else {
-    //   waitListData.push(req.body);
-    //   res.json(false);
-    // }
   });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  // app.post("/api/clear", function() {
-  //   // Empty out the arrays of data
-  //   tableData = [];
-  //   waitListData = [];
-
-  //   console.log(tableData);
-  // });
+  
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,32 +5,17 @@ module.exports = function(app) {
   app.get('/', function (req, res) {
     res.render('home');
   });
+  
+  app.get("/home", function(req, res) {
+    res.render('home');
+  });
 
   app.get("/survey", function(req, res) {
     res.render('survey');
   });
-
-
-
-  app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-
-  // app.get("/survey", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/survey.html"));
-  // });
-
-  app.get("/view", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/view.html"));
-  });
-
-  app.get("/test", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/test.html"));
-  });
-
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.render('home');
   });
 
 };
